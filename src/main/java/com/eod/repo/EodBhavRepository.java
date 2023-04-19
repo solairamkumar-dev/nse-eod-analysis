788e4631adec3e7d78ac5dc2650e70defc562ae7
Ram <solairam27@gmail.com>
2023-04-19 20:28:39 +0530

    adding RSI filter for inside candle

--- src/main/java/com/eod/repo/EodBhavRepository.java
+++ src/main/java/com/eod/repo/EodBhavRepository.java
@@ -9,10 +9,12 @@ import org.springframework.stereotype.Repository;
 
 import com.eod.entities.EodBhavData;
 
+
 @Repository
 public interface EodBhavRepository extends JpaRepository<EodBhavData, Long> {
 	@Query(value = "select t.id, t.symbol, t.series, t.open, t.high, t.low, t.close, t.last, t.prevclose, t.tottrdqty, t.tottrdval, t.timestamp, t.totaltrades, \r\n"
 			+ "t.isin from historical_data_eod t where t.timestamp = :date and t.series= :series and t.tottrdqty > :volume", nativeQuery = true)
 	public List<EodBhavData> getBhavData(@Param("date")String date, @Param("volume") long volume, @Param("series") String series);
 
+	
 }
